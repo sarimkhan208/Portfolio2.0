d4fca914b7511eaf1dac2e3d3ff3873dafa15210
sarimkhan208 <sarimkhan7275@gmail.com>
2023-06-12 17:11:19 +0530

    basic setup done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-red-100 text-xl ' >
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default App
